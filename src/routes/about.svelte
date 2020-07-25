6219d08e100e52556d26c557e3b02928875c4863
ndo@$(hostname) <yo@ndo.dev>
2020-07-25 16:41:44 +0200

    update: build script / about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,11 +1,8 @@
 <svelte:head>
-	<title>About • Svelte Hacker News</title>
+	<title>About • dev.news</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>About this app</h1>
 
-<p>This is a simple Hacker News clone, built with <a href="https://sapper.svelte.dev">Sapper</a>, an application framework for <a href="https://svelte.dev">Svelte</a>.</p>
+<p>This is a simple Hacker News clone, built with <a href="https://sapper.svelte.dev">Sapper</a>, an application framework for <a href="https://svelte.dev">Svelte</a>. It is based upon the sample HackerNews client published by Svelte themselves available [here](https://github.com/sveltejs/hn.svelte.dev) with the addition of some Navs and ProductHunt and dev.to News Feeds.</p>
 
-<p>Svelte is a new kind of framework, one that compiles your component templates into fast, compact JavaScript — either client-side or server-side. You can read more about the design and philosophy in the <a href="https://svelte.dev/blog/svelte-3-rethinking-reactivity">introductory blog post</a>.</p>
-
-<p>We're using <a href="https://github.com/davideast/hnpwa-api">hnpwa-api</a> as a backend. The app is hosted on <a href="https://cloud.google.com/run/">Cloud Run</a>, using <a href="https://www.cloudflare.com/">Cloudflare</a> for the CDN. <a href="https://github.com/sveltejs/hn.svelte.dev">The source code is here</a>.</p>
